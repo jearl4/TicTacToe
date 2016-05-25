03b0af2cbfcbe4cea448ed089a66e52e2303bbcd
J.T. Earl <jtearl188@gmail.com>
2016-05-25 17:16:17 -0400

    Added method to check for winner, print board index, print board, check if spot is taken, and see if spot is in range

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -36,18 +36,52 @@ public class TicTacToe {
 	public boolean withinRange(int number) {
 		return number > 0 && number < board.length + 1;
 	}
-	
+
 	// check if spot is taken
-	public boolean isSpotTaken(int number){
-		return board[number-1] != '-';
+	public boolean isSpotTaken(int number) {
+		return board[number - 1] != '-';
+	}
+
+	public void printBoard() {
+		System.out.println();
+		for (int i = 0; i < board.length; i++) {
+			if (i % 3 == 0 && i != 0) {
+				System.out.println();
+				System.out.println("-------------");
+			}
+			System.out.println(" | " + board[i]);
+		}
+		System.out.println();
+	}
+
+	public void printIndexBoard() {
+		System.out.println();
+		for (int i = 0; i < board.length; i++) {
+			if (i % 3 == 0 && i != 0) {
+				System.out.println();
+				System.out.println("-------------");
+			}
+			System.out.println(" | " + (i + 1));
+		}
+		System.out.println();
+	}
+
+	/**
+	 * @return true if winner
+	 */
+	public boolean isThereAWinner() {
+		boolean diagonalsAndMiddle = rightDi() || leftDi() || middleRow() || secondCol() && board[4] != '-';
+		boolean topAndFirst = topRow() || firstCol() && board[0] != '-';
+		boolean bottomAndThird = bottomRow() || thirdRow() && board[8] != '-';
+		if (diagonalsAndMiddle) {
+			this.winner = board[4];
+		} else if (topAndFirst) {
+			this.winner = board[0];
+		} else if (bottomAndThird) {
+			this.winner = board[8];
+		}
+		return diagonalsAndMiddle || topAndFirst || bottomAndThird;
 	}
 	
-	public void printBoard(){
-		System.out.println("Attempting to create...");
-		System.out.println("| - | - | - |");
-		System.out.println("-------------");
-		System.out.println("| - | - | - |");
-		System.out.println("-------------");
-		System.out.println("| - | - | - |");
-	}
+	
 }
