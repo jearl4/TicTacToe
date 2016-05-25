b5b9a266a944424cd2f5d4292d9fce48c88a4121
J.T. Earl <jtearl188@gmail.com>
2016-05-25 18:19:50 -0400

    Created pickSpot() to randomly pick a spot to play

--- src/tictactoe/AI.java
+++ src/tictactoe/AI.java
@@ -1,5 +1,19 @@
 package tictactoe;
 
-public class AI {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class AI {
+	public int pickSpot(TicTacToe game) {
+		ArrayList<Integer> choices = new ArrayList<Integer>();
+		for (int i = 0; i < 9; i++) {
+			// if slot isn't taken add it as choice
+			if (game.board[i] == '-') {
+				choices.add(i + 1);
+			}
+		}
+		Random rand = new Random();
+		int choice = choices.get(Math.abs(rand.nextInt() % choices.size()));
+		return choice;
+	}
 }
